f48610c091ca5aba0a36dcffee5f29671c5a8e1a
cataML <catelilungo@gmail.com>
2025-06-18 13:44:13 +0300

    Modified counselling

--- counselling/views.py
+++ counselling/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'counselling/index.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Thank you for contacting us.')
+            return redirect('counselling')
+    else:
+        form = ContactForm()
+        return render(request, 'counselling/index.html', {'form': form})
 
 
-def about(request):
-    return render(request, 'counselling/about.html')
